f7134d9126094b039a9fc24c6a2ba8ab08b81d22
Markus Reinhardt <tuxxilla@gmail.com>
2024-07-09 09:04:24 +0200

    Change sqlite implementation

--- config/database.go
+++ config/database.go
@@ -3,14 +3,15 @@ package config
 import (
 	"log"
 
-	"gorm.io/driver/sqlite"
+	_ "github.com/ncruces/go-sqlite3/embed"
+	"github.com/ncruces/go-sqlite3/gormlite"
 	"gorm.io/gorm"
 )
 
 func ConnectToDB() *gorm.DB {
 	var err error
 
-	db, err := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+	db, err := gorm.Open(gormlite.Open("gorm.db"), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Error connecting to database. Error: ", err)
 	}
