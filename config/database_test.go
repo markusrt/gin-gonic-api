7535e14e2279233d1383337ab3dabf17379d7b60
Markus Reinhardt <mreinhardt@vmware.com>
2024-07-09 08:57:23 +0200

    Fix test again

--- config/database_test.go
+++ config/database_test.go
@@ -10,5 +10,5 @@ func Test_ConnectToDB_DoesNotReturnNil(t *testing.T) {
 	db := ConnectToDB()
 
 	assert.Equal(t, db.Error, nil, "Success Connect to Database")
-	assert.Nil(t, db)
+	assert.NotNil(t, db)
 }
